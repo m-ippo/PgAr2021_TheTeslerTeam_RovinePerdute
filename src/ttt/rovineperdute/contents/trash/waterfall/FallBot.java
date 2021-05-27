0cc22d600c51ee6298f98ee6379c3ec5f70162c4
ThatCommand <g.vianello002@studenti.unibs.it>
2021-05-27 22:11:23 +0200

    Non funziona più, GLHF

--- src/ttt/rovineperdute/contents/trash/waterfall/FallBot.java
+++ src/ttt/rovineperdute/contents/trash/waterfall/FallBot.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package ttt.rovineperdute.contents.waterfall;
+package ttt.rovineperdute.contents.trash.waterfall;
 
 import java.util.Iterator;
 import ttt.rovineperdute.graph.Node;
